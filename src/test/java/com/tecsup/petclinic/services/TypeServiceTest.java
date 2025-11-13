b643ae595c7c9ced93c259d85b71bcded6e6be3d
luistoledolafuente <luismiguel.tlf@gmail.com>
2025-11-13 16:31:08 -0500

    test busqueda por id y name

--- src/test/java/com/tecsup/petclinic/services/TypeServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/TypeServiceTest.java
@@ -23,8 +23,43 @@ public class TypeServiceTest {
     private TypeService typeService;
 
     /**
-     *
+     * Buscar tipo por ID
      */
+    @Test
+    public void testFindTypeById() {
+
+        Integer ID = 1;
+        String NAME_EXPECTED = "cat";
+
+        Type type = null;
+
+        try {
+            type = this.typeService.findById(ID);
+        } catch (TypeNotFoundException e) {
+            fail(e.getMessage());
+        }
+
+        log.info("TYPE FOUND: " + type);
+
+        assertNotNull(type);
+        assertEquals(NAME_EXPECTED, type.getName());
+    }
+
+    /**
+     * Buscar tipo por nombre
+     */
+    @Test
+    public void testFindTypeByName() {
+
+        String FIND_NAME = "dog";
+        int SIZE_EXPECTED = 1;
+
+        List<Type> types = this.typeService.findByName(FIND_NAME);
+
+        assertEquals(SIZE_EXPECTED, types.size());
+        assertEquals(FIND_NAME, types.get(0).getName());
+    }
+
     @Test
     public void testFindAllTypes() {
 
