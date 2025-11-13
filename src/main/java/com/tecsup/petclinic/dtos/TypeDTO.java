321552e809924564382db0312709ccea97c60e58
lynmad05 <ailyn.medina@tecsup.edu.pe>
2025-11-13 16:50:36 -0500

    DTO de type y creación de archivo TypeMapper

---
+++ src/main/java/com/tecsup/petclinic/dtos/TypeDTO.java
@@ -0,0 +1,13 @@
+package com.tecsup.petclinic.dtos;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TypeDTO {
+
+    private Integer id;
+    private String name;
+}
---
+++ src/main/java/com/tecsup/petclinic/mapper/TypeMapper.java
@@ -0,0 +1,4 @@
+package com.tecsup.petclinic.mapper;
+
+public class TypeMapper {
+}
